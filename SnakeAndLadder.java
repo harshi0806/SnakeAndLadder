eef1dce8ed2a6b898b4ec59362fba64ba503eb32
harshi0806 <harshitashrivatav121@gmail.com>
2020-08-01 13:26:24 +0530

    added:UC-1 initialized with position0 to player1

---
+++ SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package SnakeAndLadder;
+
+public class SnakeAndLadder {
+
+	//initialized player1 at position 0
+	int userPosition = 0;
+
+	public static void main(String args[]) {
+		SnakeAndLadder player1 = new SnakeAndLadder();
+	}
+}
