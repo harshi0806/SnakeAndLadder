d2d9c2b1750496930a3078249928196eb79a49d2
harshi0806 <harshitashrivatav121@gmail.com>
2020-08-02 22:00:25 +0530

    added:UC-7 Continue with player2

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,16 +1,45 @@
 package SnakeAndLadder;
 import java.util.*;
 
-public class SnakeAndLadder {
+public class SnakeAndLadder implements IPlayersPlay {
 
-	//initialized player1 at position 0
-	private int userPosition = 0;
-	//intialize count to keep check on no. of throws to win the game
-	private int count = 0;
+	//instance variables
+	private LinkedList<PlayersPlay> players;
+	private Map<String, PlayersPlay> playersMap;
+
+	public SnakeAndLadder() {
+		players = new LinkedList<>();
+		playersMap = new HashMap<>();
+	}
+
+	public void addPlayersPlay(String player) {
+		PlayersPlay playersPlay = new PlayersPlay();
+		players.add(playersPlay);
+		playersMap.put(player, playersPlay);
+	}
+
+	public void startGame() {
+		for(int i = 0; i < players.size(); i++)
+		{
+			PlayersPlay playersPlay = players.get(i);
+			playersPlay.setUserPosition(this.startGame(this.getUserPosition()));
+			playersPlay.setCount(this.startGame(this.getCount()));
+			System.out.println(playersPlay);
+		}
+	}
+
+	public int getUserPlay(String player) {
+		return playersMap.get(player).getUserPosition();
+		return playersMap.get(player).getCount();
+	}
 
 	public static void main(String args[]) {
-		SnakeAndLadder player1 = new SnakeAndLadder();
-		player1.startGame();
+		//Create object of Queue for multiple players
+		IPlayersPlay SnakeAndLadder = new SnakeAndLadder();
+		//Add players to end of Queue
+		SnakeAndLadder.addPlayersPlay("player1");
+		SnakeAndLadder.addPlayersPlay("player2");
+		SnakeAndLadder.startGame();
 	}
 
 	//function for rolling a dice
@@ -21,11 +50,10 @@ public class SnakeAndLadder {
 	}
 
 	//function to start the game
-	public void startGame() {
+	public void startGame(PlayersPlay playersPlay) {
 		System.out.println("Let's start the game!");
 		//iterate till the player reaches position100
-		while(this.getUserPosition() < 100)
-		{
+		while(this.getUserPosition() < 100) {
 			//rolling a dice
 			int diceRoll = this.rollDice();
 			this.setCount(this.getCount() + 1);
@@ -72,9 +100,33 @@ public class SnakeAndLadder {
 		}
 	}
 
+   /**
+     * This will generate option to check
+     * 0 - No play, 1 - ladder, 2 - snake bite
+     */
+   public int checkOption() {
+      Random rand = new Random();
+      int option = rand.nextInt(3);
+      return option;
+   }
+}
 	/**
-	  * @return the position
+	  * created another class PlayerPlay
 	  */
+class Player {
+	public String player;
+	public int userPosition;
+	public int count;
+
+	public Player() {
+	}
+
+	public PlayersPlay(String player) {
+		this.player = player;
+		userPosition = 0;
+		count = 0;
+	}
+
 	public int getUserPosition() {
 		return userPosition;
 	}
@@ -100,16 +152,14 @@ public class SnakeAndLadder {
 	public void setCount(int count) {
 		this.count = count;
 	}
-
-	/**
-	  * This will generate option to check
-	  * 0 - No play, 1 - ladder, 2 - snake bite
-	  */
-	public int checkOption() {
-		Random rand = new Random();
-		int option = rand.nextInt(3);
-		return option;
-	}
 }
+
+interface IPlayersPlay {
+	public void addPlayersPlay(String player);
+	public void startGame();
+	public void getUserPlay(String player);
+}
+
+
 
 
